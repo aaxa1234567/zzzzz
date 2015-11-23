fd70fc06d648cdf1767a509e4a76a88990237f81
qianfeng <qianfeng@qunaerqunaer.local>
2015-11-23 15:08:46 +0800

    v1

---
+++ sssss/sssss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sssss
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 ZZF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sssss/sssss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sssss
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 ZZF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
